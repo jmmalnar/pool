f87dffe97bb8839e0ba394857d3c15b784639595
Jason Malnar <jmmalnar@utexas.edu>
2018-11-05 08:08:40 -0600

    Finished Chartkicks graphs for the Readings

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root "pools#index", as: :authenticated_root
+  end
+
   root "pages#welcome", page: "home"
   get "/pages/:page" => "pages#show"
 end
